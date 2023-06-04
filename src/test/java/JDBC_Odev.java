fa1a925cd79fb8010bfbc0e7fd6209a1a9041c8e
Merve Nur Tasan <127309624+mervenurtasann@users.noreply.github.com>
2023-06-04 15:34:20 +0300

    add

--- src/test/java/JDBC_Odev.java
+++ src/test/java/JDBC_Odev.java
@@ -1,5 +1,6 @@
 import utulities.ReusableMethods;
 
+import javax.swing.plaf.nimbus.State;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -29,9 +30,32 @@ public class JDBC_Odev {
         ResultSet rs = st.executeQuery(query);
 
         rs.next();
-        System.out.println(rs.getInt(" is_active") + " \n " + rs.getInt("is_system") + " \n " + rs.getInt("is_superadmin") + " \n " + rs.getString("created_at"));
+        System.out.println(rs.getInt("is_active") + " \n " + rs.getInt("is_system") + " \n " + rs.getInt("is_superadmin") + " \n " + rs.getString("created_at"));
 
-        rs.close();
-        st.close();
+        System.out.println("------------------------------------------------------------------");
+
+        /*
+         2- Patients tablosundaki ismi 'a' ile baslayan hastalarin patient_name, age, mobileno, email bilgilerini listeleyiniz.
+         */
+
+        query = "select patient_name,mobileno,email from heallife_hospitaltraining.patients where patient_name LIKE 'a%'";
+
+        rs = st.executeQuery(query);
+        while (rs.next()) {
+            System.out.println(rs.getString("patient_name") + " \n " + rs.getString("mobileno") + " \n " + rs.getString("email"));
+        }
+
+        System.out.println("------------------------------------------------------------------");
+
+        /*
+           3- visitors_book tablosundan purpose bilgisi 'Inquiry' olan ziyaretcilerin isimlerini ters sirali listeleyiniz.
+         */
+
+        query = "select name from heallife_hospitaltraining.visitors_book where purpose='Inquiry' order by name DESC";
+
+        rs = st.executeQuery(query);
+        while (rs.next()) {
+            System.out.println(rs.getString("name"));
+        }
     }
 }
